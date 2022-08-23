8aea91bbc34d46f937b76b1407cc657c0f7353f1
nattawat001 <108708210+nattawat001@users.noreply.github.com>
2022-08-23 11:19:55 +0700

    update

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 public class Player : MonoBehaviour
 {
     public AudioClip firearm;
     AudioSource asource;
+    int hp = 100;
+    image imgHP;
     
     // Start is called before the first frame update
     void Start()
     {
         asource = GetComponent<AudioSource>();
+        imgHP = gameObject.Find("HP").GetComponent<image>();
     }
 
     // Update is called once per frame
@@ -18,6 +22,13 @@ public class Player : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             asource.PlayOneShot(firearm);
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Zombie"))
+        {
+            hp -= 20;
         }
     }
 }
